97842f8777041917c8c9b1028697def578f18298
Ruben Taelman <rubensworks@gmail.com>
2020-09-27 13:45:34 +0200

    Allow args to be forwarded to commit

--- lib/commit.sh
+++ lib/commit.sh
@@ -4,9 +4,23 @@
 # Validate input args
 if [[ $# -eq 0 ]] ; then
     echo "Error: Missing commit parameters."
-    echo "  Usage: gmr commit 'my-commit-message'"
+    echo "  Usage: gmr commit -m \"my-commit-message\""
     exit 1
 fi
+
+remainingargs=''
+for i in "$@"; do 
+    case "$i" in  
+         *\ * )
+		       i="${i//\\/\\\\}"
+		       remainingargs="$remainingargs \"${i//\"/\\\"}\""
+               ;;
+           *)
+               remainingargs="$remainingargs $i"
+               ;;
+    esac
+done
+#echo $remainingargs
 
 # Commit in all repos
 cat .gmr-config.txt | while read repo
@@ -14,7 +28,7 @@ do
    pushd $repo > /dev/null
    
    echo -e "\033[1m\033[34m$(basename $repo)\033[0m"
-   git commit -m "$1"
+   git commit "$@"
 
    popd > /dev/null
 done
