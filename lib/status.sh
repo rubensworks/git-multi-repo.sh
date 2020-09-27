1ab23793765cef876c8fe129a6585e114e356dfa
Ruben Taelman <rubensworks@gmail.com>
2020-09-27 12:13:47 +0200

    Show staged changes in status

--- lib/status.sh
+++ lib/status.sh
@@ -5,7 +5,8 @@ cat .gmr-config.txt | while read repo
 do
    pushd $repo > /dev/null
    branch=$(git rev-parse --abbrev-ref HEAD)
-   unstaged=$([[ ! -z "$(git diff)" ]] && echo -e "\033[91m- Unstaged changes\033[0m" || echo "")
-   echo -e "\033[1m\033[34m$(basename $repo)\033[0m - $branch $unstaged"
+   unstaged=$([[ ! -z "$(git diff)" ]] && echo -e " \033[91m- Unstaged changes\033[0m" || echo "")
+   staged=$([[ ! -z $(git status -s) ]] && echo -e " \033[92m- Staged changes\033[0m" || echo "")
+   echo -e "\033[1m\033[34m$(basename $repo)\033[0m - $branch$staged$unstaged"
    popd > /dev/null
 done
