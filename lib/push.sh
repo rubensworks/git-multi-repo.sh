00d058eb73c8923baacedc0d1807b4dc9bc76adc
Ruben Taelman <rubensworks@gmail.com>
2020-09-27 13:48:51 +0200

    Allow args to be forwarded to push

--- lib/push.sh
+++ lib/push.sh
@@ -6,7 +6,7 @@ do
    pushd $repo > /dev/null
    
    echo -e "\033[1m\033[34m$(basename $repo)\033[0m"
-   git push origin $(git rev-parse --abbrev-ref HEAD)
+   git push "$@" origin $(git rev-parse --abbrev-ref HEAD)
 
    popd > /dev/null
 done
