be590edd32f7f84da1f95858717e374b98814e7f
Ruben Taelman <rubensworks@gmail.com>
2020-09-27 12:34:18 +0200

    Show pending commits in status

--- lib/status.sh
+++ lib/status.sh
@@ -5,8 +5,14 @@ cat .gmr-config.txt | while read repo
 do
    pushd $repo > /dev/null
    branch=$(git rev-parse --abbrev-ref HEAD)
-   unstaged=$([[ ! -z "$(git diff)" ]] && echo -e " \033[91m- Unstaged changes\033[0m" || echo "")
-   staged=$([[ ! -z $(git status -s) ]] && echo -e " \033[92m- Staged changes\033[0m" || echo "")
-   echo -e "\033[1m\033[34m$(basename $repo)\033[0m - $branch$staged$unstaged"
+   unstaged=$([[ ! -z "$(git diff)" ]] && echo -e " - \033[91mUnstaged changes\033[0m" || echo "")
+   staged=$([[ ! -z $(git diff --cached) ]] && echo -e " - \033[92mStaged changes\033[0m" || echo "")
+   unpushedcommits=$(git log origin/master-1.12..HEAD --pretty=oneline | wc -l | tr -d ' ')
+   if [ "$unpushedcommits" != "0" ]; then
+	   unpushedcommits=" - \033[93mPending commits: $unpushedcommits\033[0m"
+   else
+	   unpushedcommits=""
+   fi
+   echo -e "\033[1m\033[34m$(basename $repo)\033[0m - $branch$unpushedcommits$staged$unstaged"
    popd > /dev/null
 done
